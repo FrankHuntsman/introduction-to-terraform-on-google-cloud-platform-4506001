0bca9550934dbe0001adfc8adfebffe3bb52ee26
FrankHuntsman <87278093+FrankHuntsman@users.noreply.github.com>
2024-11-11 14:51:10 -0500

    website

--- main.tf
+++ main.tf
@@ -31,20 +31,6 @@ module "app_network" {
  ]
 }
 
-
-resource "google_compute_network" "app" {
-  name                    = var.network_name
-  auto_create_subnetworks = false
-}
-
-resource "google_compute_subnetwork" "app" {
-  name          = var.network_name
-  ip_cidr_range = var.network_ip_range
-  region        = var.region
-  network       = google_compute_network.app.id
-  }
-
-
 data "google_compute_image" "ubuntu" {
   most_recent = true
   project     = var.image_project
@@ -54,6 +40,8 @@ data "google_compute_image" "ubuntu" {
 resource "google_compute_instance" "blog" {
   name         = var.app_name
   machine_type = var.image_family
+
+  tags = ["{var.network_name}-web"]
 
   
   boot_disk {
@@ -68,5 +56,7 @@ resource "google_compute_instance" "blog" {
     }
   }  
 
-allow_stopping_for_update = true
+ metadata_startup_script = "apt -y update; apt -y install nginx; echo ${var.app-name} > /var/www/html/index.html"
+
+ allow_stopping_for_update = true
 }
